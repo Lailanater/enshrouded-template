c4e33baace6994dadd772ae9138dca6916707b40
Christopher Lail <chrislail@protonmail.com>
2025-10-05 17:28:50 -0700

    Generate random passwords if the file does not already exist

--- start.sh
+++ start.sh
@@ -3,4 +3,14 @@
 DIR=$(realpath $(dirname "$0"))
 cd "$DIR"
 
+function gen_password() {
+  if [[ ! -f "$1" ]]; then
+    tr -dc 'A-Za-z0-9!?%=' < /dev/urandom | head -c 16 > "$1"
+  fi
+}
+
+gen_password $DIR/secrets/admin_password.txt
+gen_password $DIR/secrets/friend_password.txt
+gen_password $DIR/secrets/guest_password.txt
+
 docker compose up
